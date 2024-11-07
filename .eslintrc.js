b4a4129a7472a1e2867c809838996897e7bd9912
CosLynx AI <drixisgod@gmail.com>
2024-11-07 18:01:30 +0530

    generated file: `.eslintrc.js`

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['next/core-web-vitals', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+  },
+};
